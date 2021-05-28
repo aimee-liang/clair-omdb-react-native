c185dc372274605e8502863519fc3254f3ac9213
aimee-liang <aimeeeliang@gmail.com>
2021-05-28 12:02:04 -0400

    Outline of SearchBar.jsx functionality

---
+++ SearchBar.jsx
@@ -0,0 +1,11 @@
+/*
+
+SearchBar should be a form input field, with a placeholder text
+
+This component will need useState hook: initial value of movie title as an empty string
+    Once user begins typing, the value will change to the e.target.value
+        On tap/click/enter of keyboard event, the value will return to an empty string
+
+The data entered into SearchBar will be passed up to the parent component, which will handle this
+
+*/
