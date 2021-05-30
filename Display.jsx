9ada5c37e7b666a21ffde415fba1dcfdaf86e17f
aimee-liang <aimeeeliang@gmail.com>
2021-05-30 17:17:08 -0400

    Text in place of H tag for movie title

--- Display.jsx
+++ Display.jsx
@@ -1,6 +1,7 @@
 /*
 
-Display should be displayed underneath SearchBar
+Display should be displayed underneath SearchBar. 
+    It will need three variables to calculate for Mean, Standard Deviation, and Rotten Tomatoes score
 
 */
 
@@ -8,19 +9,27 @@ import React, {useState} from 'react'
 import {StyleSheet, View, Image} from 'react-native'
 
 const styles = StyleSheet.create({
-    container: {
-        
+    titleText: {
+        fontSize: 18,
+        fontWeight: "bold"
     }
 })
 
 const Display = (props) => {
     /* should display Box Office Mean, Box Office Standard Deviation, and Median Rotten Tomatoes Score */
+
+    const boxOfficeMean
+
+    const boxOfficeStandardDeviation 
+
+    /* iterate through the object at the Ratings Key. If the score is between 0 - 10, multiply by 10. Otherwise, if  */
+    const medianRTScore
+
     return(
         <>
             <Image>{props.movieInDisplay.poster}</Image>
-            <View>
-                
-            </View>
+            <Text style={titleText}>{/* movie title goes here? */}</Text>
+            <View></View>
         </>
     )
 }
