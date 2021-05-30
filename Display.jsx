9b8a3462a6a1a16b49d676a8764951f355896886
aimee-liang <aimeeeliang@gmail.com>
2021-05-30 14:07:20 -0400

    Working on Display.jsx

--- Display.jsx
+++ Display.jsx
@@ -7,17 +7,17 @@ Display should be displayed underneath SearchBar
 import React, {useState} from 'react'
 import {StyleSheet, View} from 'react-native'
 
-// const styles = StyleSheet.create({
-//     container: {
+const styles = StyleSheet.create({
+    container: {
         
-//     }
-// })
+    }
+})
 
 const Display = (props) => {
     /* should display Box Office Mean, Box Office Standard Deviation, and Median RT Score */
     return(
         <View>
-        
+            
         </View>
     )
 }
