7f84ac18796935871d6120aedfd2cf85d46532fd
aimee-liang <aimeeeliang@gmail.com>
2021-05-31 14:19:15 -0400

    Cleaning up Result.jsx

--- Result.jsx
+++ Result.jsx
@@ -5,7 +5,7 @@ Result should render after user searches to confirm if this is the movie they wa
 */
 
 import React from 'react'
-import {StyleSheet, View, Button, Text, Image, TextInput, Modal} from 'react-native'
+import {StyleSheet, View, Button, Text, Image, TextInput} from 'react-native'
 
 const styles = StyleSheet.create({
     container: {
@@ -33,24 +33,23 @@ const styles = StyleSheet.create({
 
 export default function Result(props){
 
-    // const [movieInResult, setMovieInResult] = useState([])
-    // const [added, setAdded] = useState(false)
-
-    // const fetchMovie = (link) => {
-    //     fetch(`${link}`)
-    //         .then(response => response.json())
-    //         .then(data => setMovieInResult(data)) /* after fetching the movie we searched, this will be passed as props to Result.jsx */
-    // }
+    const [added, setAdded] = useState(false)
 
     const resultPressHandler = () => {
         props.addMovieToDisplay(props.movieInResult)
-        setAdded(true)
+        setAdded(!added)
     }
 
     /* Confirmation alert for UX: user is aware they've saved this movie to their search list */
     const confirmationAlert = () => {        
-        return "You've added this to your list!"
-    }
+        return Alert.alert(
+            "Success!",
+            "You have saved this movie",
+            [
+                { text: "OK", onPress: () => console.log("OK Pressed") }
+            ],
+            { cancelable: true }
+    )}
 
 
 
@@ -61,11 +60,7 @@ export default function Result(props){
             <Text style={styles.movieTitleText}>{props.movieInResult["Title"]}</Text>
             <Text style={styles.yearText}>{props.movieInResult["Year"]}</Text>
             <Button onPress={resultPressHandler} title="Add to Search List" />
-            {/* {added ? 
-                <Modal>{confirmationAlert}</Modal> 
-                : 
-                null
-            } */}
+            {added ? confirmationAlert() : null}
         </View>
         </>
     )
