1c17310246f46437f85b6a7681f6223b471ead00
aimee-liang <aimeeeliang@gmail.com>
2021-06-06 22:34:25 -0400

    updateIfErrors() invoked in finishedFetching()

--- Home.jsx
+++ Home.jsx
@@ -16,7 +16,7 @@ export default function Home(props){
 
     const [movieInResult, setMovieInResult] = useState([]) /* this saves the movie in Result after fetching */
     const [doneFetching, setDoneFetching] = useState(false) /* flag variable - if done fetching, will be set to the opposite value of initial state */
-    const [hasErrors, setHasErrors] = useState(false) /* flag variable set to boolean value */
+    const [hasErrors, setHasErrors] = useState(false) /* flag variable set to boolean value which will toggle true if there is an error in movieInResult var */
     const movieLink = 'http://www.omdbapi.com/?apikey=29144b52&t=' /* link to API with key to later concat */
 
     const editMovieTitle = (movieTitle) => {
@@ -32,10 +32,10 @@ export default function Home(props){
         finishedFetching()
     }
 
-    /* handles setting state of doneFetching */
+    /* handles setting state of doneFetching and if there are errors*/
     const finishedFetching = () => {
         setDoneFetching(!doneFetching)
-        helperFn() /* to do: clean up */
+        updateIfErrors()
     }
 
     const updateIfErrors = () => {
@@ -59,7 +59,8 @@ export default function Home(props){
     return (
         <View style={styles.container}>
             <SearchBar editMovieTitle={editMovieTitle}/>
-            {movieInResult.hasOwnProperty("Error") ? alertMessage() : null}
+            {hasErrors ? alertMessage() : null}
+            {/* {movieInResult.hasOwnProperty("Error") ? alertMessage() : null} */}
             {movieInResult.hasOwnProperty("Title") ? <Result movieInResult={movieInResult} addMovieToDisplayComponent={props.addMovieToDisplayComponent} doneFetching={doneFetching} /> 
             : 
             null}
